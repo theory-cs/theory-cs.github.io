e3995baa8a317f475fd3987710fcd4f902662eff
Joe Politz <joe.politz@gmail.com>
2021-08-27 14:40:08 -0700

    script for getting the fine-grained outcomes from outcomes.json:

---
+++ outcomes-list.py
@@ -0,0 +1,12 @@
+import json
+outcomes = json.loads(open("outcomes.json").read())
+
+low_levels = []
+for (k, v) in outcomes.items():
+  for (k2, v2) in v["Children"].items():
+    for k3 in v2["Children"].keys():
+      low_levels.append(k3)
+
+print(len(low_levels))
+
+for outcome in low_levels: print(outcome)
