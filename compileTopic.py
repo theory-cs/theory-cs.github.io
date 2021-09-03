729de65afc7f7add7f9ff08e574d754bec530ec4
Aiko Coanaya <aikocoanaya@gmail.com>
2021-09-02 20:28:13 -0700

    Update compileTopic.py
    Added comments to the script to better understand the code.

--- compileTopic.py
+++ compileTopic.py
@@ -14,8 +14,10 @@ for (k, v) in outcomes.items():
       low_levels.append(k3.replace(" ", "-").lower()) 
 
 
+# Create a dictionary
 lowLevelsDict = {}
 for line in low_levels:
+    # Each dictionary has the low_levels as key and empty array as value
     lowLevelsDict[line] = []
 
 
@@ -23,22 +25,34 @@ for line in low_levels:
 directory = "notes/testDir"
 for filename in os.listdir(directory):
     
+
+    # Get the second line of each file and clean the string
     particularLine = linecache.getline(directory + "/" + filename, 2).replace("%! small-outcomes: ", "").replace("\n", "")
     # print(particularLine)
     
+
+    # Split small outcomes with the delimiter ", " into a list
     li = list(particularLine.split(", "))
     for element in li:
+        # lowercase them and replace whitespace with dashes (to make them uniform)
         test = element.replace(" ", "-").lower()
+        # add that tex filename to the dictionary
         lowLevelsDict[test].append(filename)
 
+# UNCOMMENT if want to see how the dictionary looks
+# print(lowLevelsDict)
 
-for line in lowLevelsDict:
-    if(len(lowLevelsDict[line]) > 0):
+
+# Iterate through the dict
+for key in lowLevelsDict:
+    # Only run if the key has elements
+    if(len(lowLevelsDict[key]) > 0):
+        # Create each tex file
         result = "\input{../../resources/lesson-head.tex}"
 
         result += "\n"
 
-        for tex in lowLevelsDict[line]:
+        for tex in lowLevelsDict[key]:
             # TO DO: CHANGE testDir TO activity-snippets
             result += "\input{../testDir/" + tex + "}"
             result += "\n"
@@ -47,10 +61,7 @@ for line in lowLevelsDict:
 
         result += "\end{document}"
 
-
-
-
-        resultFile = open("notes/lessons/"+ line + ".tex", "w")
+        resultFile = open("notes/lessons/"+ key + ".tex", "w")
         resultFile.write(result)
 
         resultFile.close()
