9f615e2cf5a567e074b435bb0d118d066d970295
Akanksha Pandey <78574543+akanksha-maker-ucsd@users.noreply.github.com>
2021-09-10 12:18:44 -0700

    Update weeklyCompileTopic.py
    uses unit-setting.json to order snippets by week

--- weeklyCompileTopic.py
+++ weeklyCompileTopic.py
@@ -12,6 +12,8 @@ import linecache
 import json
 outcomes = json.loads(open("outcomes.json").read())
 
+# returns unit-settings JSON file as a dictionary
+unitData = json.loads(open("unit-settings.json").read())
 
 # Credit: Professor Politz code from outcomes-list.py
 low_levels = []
@@ -36,18 +38,34 @@ for line in low_levels:
 def findWeek(element):
     return int(element[1])
 
+#if file in lessons directory is not found within a week on the website/unit-settings.json, then it will have 99 as a week number, 
+#so that it is sorted to the end of compiled .tex files 
+weekNumber = 99
 
 weeklyDirectory = "notes/lessons"
 for filename in os.listdir(weeklyDirectory):
     #print(filename)
     weekly = open (weeklyDirectory+"/"+filename, "r")
 
-    #get week number/order from weekly notes file- this will be the number right before .tex(length-4)
-    #convention: #.tex -> must be at the end of every lessons/ file
-    weekNumber = filename[len(filename)-5]
+   #remove .tex extension from filename
+    editFilename= filename.replace(".tex","")
+    #print("editFilename: "+editFilename)
+
+    #get week number/order from unit-settings.json file, this will be the order in which files appear on the website
+    for element in unitData:
+        for pdf in element['pdfs']:
+            if( editFilename in pdf['file']):
+                
+
+                weekNumber= unitData.index(element)+1
+
+                #debug
+                #print(pdf['file'])
+                #print(" index: "+str(unitData.index(element)+1))
+    
     
     #debug
-    #print(filename+" "+weekNumber)
+    #print(filename+" "+str(weekNumber))
     
 
     Lines = weekly.readlines()
@@ -91,8 +109,9 @@ for filename in os.listdir(weeklyDirectory):
 
                 #sort each outcome by week
                 lowLevelsDict[test].sort(key=findWeek)
+
 # UNCOMMENT if want to see how the dictionary looks
-#print(lowLevelsDict)
+print(lowLevelsDict)
 
 def write_if_different(filename, contents):
     try:
