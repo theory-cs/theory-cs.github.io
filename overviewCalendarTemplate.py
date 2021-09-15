cdee1b8b8ed79047a7a92f38fcb7e4edf0e162fa
Akanksha Pandey <78574543+akanksha-maker-ucsd@users.noreply.github.com>
2021-09-15 15:35:19 -0700

    new layout for overviewCalendar

--- overviewCalendarTemplate.py
+++ overviewCalendarTemplate.py
@@ -6,12 +6,42 @@ from userFunctions import *
 unitData = json.loads(open("unit-settings.json").read())
 websiteData = json.loads(open("website-settings.json").read())
 
+for i in range (1, len(unitData)+1):
+    boxString =""
+    
+    #set appropriate variables with information from unit-settings.json
+    weekNumber = i
+    info=""
+    if('CalendarInfo' in unitData[i-1]):
+        dates=unitData[i-1]['CalendarInfo']
+    assignments=""
+    if('Assignments' in unitData[i-1]):
+        assignments= unitData[i-1]['Assignments']
+    
+    #box heading and subheading/description
+    boxString += """<div class="box"> \n"""
+    boxString += "<h2> Week " + str(weekNumber) + "</h2>"
+    boxString += "<p> " + info + "</p>"
+    boxString += "<hr>"
+
+    #list begins
+    boxString += """<div class="column"> <dl> \n"""
+
+    boxString += "</dl>"
+    boxString += "</div>"
+    boxString += "</div><br><br>"
+
+    
+
+
+
 # Open unitTemplate html file and read it into a string 
 unitTemplate = open("overviewCalendarTemplate.html", "r")
 templateString = Template(unitTemplate.read())
 
 page_variables = site_variables.copy()
 page_variables.update(dict(
+    unitBoxes = boxString
 ))
 
 # Substitute settings unitData with appropriate variables 
