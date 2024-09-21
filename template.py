e35fe75565cefcc5a20090b1b5b07941e5977db3
miaminnes <minnes@eng.ucsd.edu>
2024-09-21 06:45:46 -0700

    Initial Fall 2024 upload

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ import json
 from user_functions import *
 
 # Template for each webpage view and Generated for the result file name
-template = ["index_template", "feedback_template", "overview_calendar_template", "overview_outcome_template", "overview_application_template", "assignment_template", "course_info_template", "office_hours_template"]
+template = ["index_template", "feedback_template", "overview_calendar_template", "overview_outcome_template", "overview_application_template", "assignment_template", "course_info_template","office_hours_template"]
 generated = ["index", "feedback", "overview_calendar", "overview_outcome", "overview_application", "assignments", "courseInfo", "office_hours"]
 
 # Substitute template 
